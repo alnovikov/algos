b424fb368e91d4e7aa2374c2670e8a85a0a2637b
Alexander Novikov <alnovikov@users.noreply.github.com>
2017-04-10 11:02:16 +0100

    added merge sort

--- sort.py
+++ sort.py
@@ -12,4 +12,41 @@ def bubble_sort(array):
     return array
 
 a = [4,9,9,3,7,1,4,5,3,6,7,1,3,5,6,3,6,7,2,5,6,8,9,3,2,1,-1,10]
-bubble_sort(a)
+print(bubble_sort(a))
+
+
+'''
+3. merge sort:
+recursively divide and sort the array
+'''
+def merge(a,b):
+    c = []
+    while len(a) > 0 and len(b) > 0:
+        if a[0] > b[0]:
+            c.append(b[0])
+            b.remove(b[0])
+        else:
+            c.append(a[0])
+            a.remove(a[0])
+    while len(a) > 0:
+        c.append(a[0])
+        a.remove(a[0])
+    while len(b) > 0:
+        c.append(b[0])
+        b.remove(b[0])
+    return(c)
+
+def merge_sort(array):
+    n = int(len(array))
+    if n == 1:
+        return array
+    if n > 1:
+        l1 = array[0:int(n/2)]
+        l2 = array[int(n/2):n]
+
+        l1 = merge_sort(l1)
+        l2 = merge_sort(l2)
+    return(merge(l1, l2))
+  
+data = [7,1,13,9,12,11,0,5]
+print(merge_sort(data))
